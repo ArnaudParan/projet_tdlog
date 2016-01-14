b51ab9b6c9279ca1c9ec9f14469da03eb44db58b
Simon Lebastard <lebastardsimon@gmail.com>
2016-01-14 19:49:55 +0100

    Correction des erreurs: affichage de paramètres de test

--- app/www/js/friends.js
+++ app/www/js/friends.js
@@ -3,9 +3,12 @@ var appFriends = angular.module('appFriends', ['onsen']);
 appFriends.controller('friend_Controller', ['$scope', '$rootScope', function($scope, $rootScope) {
   $scope.friends = localDB.get_all_friends_names_tel();
   $scope.request = ""; 
+  $scope.add_request = false;
   $scope.search_a_friend = function(req){
-	  console.log(req);
 	  $scope.friends = localDB.search_friends(req);
+  }
+    $scope.friend_request = function(){
+	  $scope.add_request() = !$scope.add_request();
   }
 }])
 

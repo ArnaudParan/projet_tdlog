d595f8921c0080b6a84fac4c09394efb11a6aba1
Simon Lebastard <lebastardsimon@gmail.com>
2016-01-14 16:45:54 +0100

    Adaptation des controleurs de friends.html aux nouveaux mocks obj

--- app/www/js/friends.js
+++ app/www/js/friends.js
@@ -1,18 +1,23 @@
 var appFriends = angular.module('appFriends', ['onsen']);
 
 appFriends.controller('friend_Controller', ['$scope', function($scope) {
-  $rootscope.friends = get_friends();
   $scope.test = true;
+  $scope.friends = localDB.get_all_friends_names_tel();
 }])
 
 appFriends.controller('research_bar', ['$scope', function($scope) {
-  $scope.request = "";
+  $scope.request = ""; 
+  //$scope.$watch($scope.request, search_a_friend_service);
 }])
 
-appFriends.directive('friendFilter', function() {
-  return {
-    restrict: 'E',
-    scope.$watch(request, function(){
-		friends = get_matching_friends(request);
-	});
-});
+/*appFriends.factory('search_a_friend_service', ['$window', function(win){
+   var msgs = [];
+   return function(msg) {
+     msgs.push(msg);
+     if (msgs.length == 3) {
+       win.alert(msgs.join("\n"));
+       msgs = [];
+     }
+   };
+}]);
+*/
--- app/www/js/global_variables.js
+++ app/www/js/global_variables.js
@@ -1,5 +1,7 @@
 var server_address = "http://server/";
 var user_mail = -1;
 var user_password = -1;
-var get_friends = mock_get_all_friends;
-var get_matching_friends = mock_get_req_friends;
+var localDB = mock.local.DB;
+
+
+

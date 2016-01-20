635b9b5d26de3d46c5a153c13ddf88997e93117e
ELOISESOULIER <eloise.soulier@gmail.com>
2016-01-20 11:30:47 +0100

    fonction create_event fonctionnelle

--- NvBaseDonnees/BDlove/views.py
+++ NvBaseDonnees/BDlove/views.py
@@ -30,7 +30,7 @@ def home(request):
 
 def exists(request):
     email = request.GET['email']
-    mdp_hashe = request.GET['mdp_hashe']
+    hashe = request.GET['mdp_hashe']
     if is_user(email,mdp_hashe):
         return HttpResponse(json.dumps({'resultat':'success'}), content_type='application/json')
     else:
@@ -38,13 +38,14 @@ def exists(request):
     
 def create_event(request):
     email = request.GET['email']
-    pwd = request.GET['mdp_hache']
+    pwd = request.GET['mdp']
+    #TODO : rajouter fonction de hachage
     latitude = request.GET['latitude']
     longitude = request.GET['longitude']
     nom = request.GET['nom']
     #date = request.GET['date']
     id_createur = request.GET['id']
-    createur0=Utilisateur.objects.filter(identifiant=id_createur).first()
+    createur0=Utilisateur.objects.filter(id=id_createur).first()
     nb_participants = int(request.GET['nb_participants'])
     if is_user(email,pwd):
         evenement=Evenement(nom=nom,date=datetime.datetime.now(),lieu_geo_lat=latitude, lieu_geo_long=longitude,createur=createur0)
@@ -52,10 +53,11 @@ def create_event(request):
         evenement.participants.add(id_createur)
         for j in range(1,nb_participants):
             id_ami = request.GET['ami{}'.format(j)]
-            participant = Utilisateur.objects.filter(identifiant=id_ami).first()
+            participant = Utilisateur.objects.filter(id=id_ami).first()
             if participant is not None:
                 evenement.participants.add(participant) 
-        return HttpResponse(json.dumps({'resultat':'success'}), content_type='application/json')
+        return HttpResponse(json.dumps({'resultat':'success','id_evenement':evenement.id } ), content_type='application/json')
+        #return HttpResponse("Bonjour %s!" evenement.id)
         
     else: 
         return HttpResponse(json.dumps({'resultat':'fail', 'error':'1002', 'message_erreur':'t\'existes pas tu peux pas créer d\'évènement bolosse'}),  content_type='application/json')
